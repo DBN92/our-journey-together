6901b1bdc73bfef92f092776bca48d398fd0079f
DBN92 <dan.bezerra@outlook.com.br>
2026-01-05 08:40:33 -0300

    feat(routing): redirecionar para /login quando casal ausente

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -65,7 +65,7 @@ const Index = () => {
       if (data?.id) {
         setCoupleId(data.id as string);
       } else {
-        navigate("/");
+        navigate("/login");
       }
     };
     checkOnboarding();
